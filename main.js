a823b2bc517944682281c7a55ef8c190a3ca1b4b
jessed18 <jessedg27@gmail.com>
2025-10-01 11:25:37 -0700

    user has to choose a celeb AND feeling

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const searchBtn = document.getElementById('search-btn');
 const celebritySelect = document.getElementById('celebrity-select');
 const feelingSelect = document.getElementById('feeling-select');
 const gifGrid = document.getElementById('gif-grid');
+
+searchBtn.addEventListener('click',() => {
+    const celebrity = celebritySelect.value;
+    const feeling = feelingSelect.value;
+
+if (! celebrity || !feeling) {
+    alert('Please select both a celebrity and a feeling.');
+    return;
+}
+
+
+
+
+})
