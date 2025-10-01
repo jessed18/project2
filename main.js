4d415413342608f8f452d4dea64b3521ad303178
jessed18 <jessedg27@gmail.com>
2025-10-01 11:20:04 -0700

    adding important elements

---
+++ main.js
@@ -0,0 +1,6 @@
+const API_KEY = 'key'
+
+const searchBtn = document.getElementById('search-btn');
+const celebritySelect = document.getElementById('celebrity-select');
+const feelingSelect = document.getElementById('feeling-select');
+const gifGrid = document.getElementById('gif-grid');
